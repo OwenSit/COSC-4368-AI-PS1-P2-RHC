e653804c53b972260ae6e1e817d3fc36a44eb3b0
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2021-02-26 13:12:50 -0600

    new updates

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def fitness_fn(x, y):
 
 
 def RHC(sp, p, z, seed):
+    local_sp = sp.copy()
     global num_sol, local_best_fitness
     num_sol = 0
 
@@ -34,21 +35,21 @@ def RHC(sp, p, z, seed):
         z1 = sign * (random.random() * 10 % z)
         z2 = sign * (random.random() * 10 % z)
 
-        # assign changes to sp:
-        sp[0] += z1
-        sp[1] += z2
+        # assign changes to local_sp:
+        local_sp[0] += z1
+        local_sp[1] += z2
 
         num_sol += 1
 
-        if not ((sp[0] >= (-2)) and (sp[1] <= 2)):
+        if not ((local_sp[0] >= (-2)) and (local_sp[1] <= 2)):
             print("x, y is out of range")
             continue
 
-        temp_fitness = fitness_fn(sp[0], sp[1])
+        temp_fitness = fitness_fn(local_sp[0], local_sp[1])
 
         if temp_fitness < local_best_fitness:
             local_best_fitness = temp_fitness
-            local_best_sp = sp.copy()
+            local_best_sp = local_sp.copy()
 
     return local_best_sp
 
@@ -67,26 +68,6 @@ if __name__ == '__main__':
 
     """The following implement is asking using for arguments, which is the slow way"""
 
-    # if len(sys.argv) != 6:
-    #     print("not enough argument!")
-    #     sys.exit()
-    #
-    # sp1 = float(sys.argv[1])
-    # sp2 = float(sys.argv[2])
-    # my_p = sys.argv[3]
-    # my_z = sys.argv[4]
-    # my_seed = sys.argv[5]
-    #
-    # if (sp1 < -2) or (sp1 > 2):
-    #     print("sp should start with range [-2,2]")
-    #     sys.exit()
-    #
-    # if (sp2 < -2) or (sp2 > 2):
-    #     print("sp should start with range [-2,2]")
-    #     sys.exit()
-    #
-    # my_sp = [sp1, sp2]
-
     for my_sp in sp_arr:
         for my_p in p_arr:
             for my_z in z_arr:
@@ -101,8 +82,10 @@ if __name__ == '__main__':
                     global_fitness.append(local_best_fitness)
                     # global_sp.append(temp_best_sp)  # store the sp value
                     print("\n\nRUNTIME #" + str(runtime_num))
-                    print("SP:", end="")
+                    print("SP:", end=" ")
                     print(my_sp, end=" ")
+                    print("generated_best_SP:", end=" ")
+                    print(temp_best_sp, end=" ")
                     print("p:", end=" ")
                     print(my_p, end=" ")
                     print("z:", end=" ")
@@ -112,7 +95,7 @@ if __name__ == '__main__':
                     print("seed:", end=" ")
                     print(my_seed, end=" ")
                     print("runtime:", end=" ")
-                    print(temp_time_taken, end=" ")
+                    print(temp_time_taken, end="sec ")
                     print("num_sol:", end=" ")
                     print(num_sol, end=" ")
                     print("fitness:", end=" ")
